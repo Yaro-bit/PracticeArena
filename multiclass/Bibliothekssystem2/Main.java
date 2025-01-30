8be3e2b122b98baac3d09d3a1cf8dcc3f0fa19a2
Yaro <piraterieforyou@proton.me>
2025-01-30 16:50:07 +0100

    Add files via upload

---
+++ multiclass/Bibliothekssystem2/Main.java
@@ -0,0 +1,23 @@
+package Bibliothekssystem2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Book book[] = {new Book("Java für orsch","daniel varga","ISBN123824",true),
+				new Book("Java für orsch 2","daniel varga","ISBN123824",true),
+				new Book("deppad schaun","yaroslav woll","ISBN123824",true),
+		};
+		Contact contact[] = {new Contact("Adolf Hitler","+432312312321","arschloch@mail.orsch", null)
+		};
+
+		for(int i = 0; i<book.length;i++) {
+			System.out.println(book[i]);
+		}
+		
+	}
+
+}
+//name = "unbekannt";
+//telno = "unbekannt";
+//email = "unbekannt";
+//adress = "unbekannt";
---
+++ multiclass/Bibliothekssystem2/Task.java
@@ -0,0 +1,10 @@
+package Bibliothekssystem2;
+import java.util.Date;
+
+public class Task {
+	String desc;
+	int prio;
+	Date expDate;
+	String status;
+	
+}
