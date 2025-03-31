d044b2945f71d823adcf3c91d9661735854743c7
Yaro <piraterieforyou@proton.me>
2025-03-31 13:40:25 +0200

    Delete basics directory

--- basics/w3s/OOP1_1.java
+++
@@ -1,8 +0,0 @@
-package w3s;
-
-class OOP1_1 {
-	public static void main(String[] args) {
-		OOP1 meinMainAlsObject = new OOP1();
-		System.out.println(meinMainAlsObject.x);
-	}
-}
--- basics/w3s/Polymorphism_Hund.java
+++
@@ -1,7 +0,0 @@
-package w3s;
-
-public class Polymorphism_Hund {
-	public void animalSound() {
-		System.out.println("Der Hund ist deppad");
-	}
-}
